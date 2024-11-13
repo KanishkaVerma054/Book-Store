3f8c3be4fcc22ae28147c1509f45ac1745c1f0b0
Kanishka Verma <kanishkav774@gmail.com>
2024-11-13 20:21:41 +0530

    Book icons added

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -3,13 +3,12 @@ import axios from 'axios';
 import Loading from '../components/Loading';
 import { Link } from 'react-router-dom';
 import { MdOutlineAddBox } from 'react-icons/md';
-import BooksTable from '../components/home/BooksTable';
 import BooksCard from '../components/home/BooksCard';
+import { TbBooks } from "react-icons/tb";
 
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [showType, setShowType] = useState('table');
 
   useEffect(() => {
     setLoading(true);
@@ -26,31 +25,19 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='p-4'>
-      <div className='flex justify-center items-center gap-x-4'>
-        <button
-          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
-          onClick={() => setShowType('table')}
-        >
-          Table
-        </button>
-        <button
-          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
-          onClick={() => setShowType('card')}
-        >
-          Card
-        </button>
-      </div>
+    <div className='p-4 h-screen'>
       <div className='flex justify-between items-center'>
-        <h1 className='text-3xl my-8'>Books List</h1>
+        <div className='flex justify-start items-center ml-4'>
+        <TbBooks className=' text-5xl'/>
+        <h1 className='text-3xl my-8'>Library</h1>
+        </div>
+        
         <Link to='/books/create'>
           <MdOutlineAddBox className='text-sky-800 text-4xl' />
         </Link>
       </div>
       {loading ? (
         <Loading />
-      ) : showType === 'table' ? (
-        <BooksTable books={books} />
       ) : (
         <BooksCard books={books} />
       )}
